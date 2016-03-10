5881e8ae62b319a88e0f47ab0d00e2e84ca074c9
Matthew <matthew.steglinski@utoronto.ca>
2016-03-10 17:53:52 -0500

    Deep clone packets before sending to avoid data overwriting PacketEvents for other players

--- src/main/java/com/sainttx/holograms/placeholders/HologramPacketAdapter.java
+++ src/main/java/com/sainttx/holograms/placeholders/HologramPacketAdapter.java
@@ -1,6 +1,7 @@
 package com.sainttx.holograms.placeholders;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
@@ -15,14 +16,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HologramPacketAdapter extends PacketAdapter {
 
     private HologramPlugin plugin;
+    private Set<UUID> sendingMetadataPacket = Collections.newSetFromMap(new ConcurrentHashMap<>());
     private Set<HologramLine> trackedPlaceholderLines = Collections.newSetFromMap(new WeakHashMap<>());
 
     public HologramPacketAdapter(Plugin plugin) {
@@ -53,15 +58,27 @@ public class HologramPacketAdapter extends PacketAdapter {
                 && PlaceholderAPI.containsPlaceholders(line.getText())) {
             trackedPlaceholderLines.add(line);
             plugin.getHologramManager().trackLine(new WrappedUpdatingTextualLine(plugin.getHologramManager(), line));
-        } else if (tracked && event.getPacketType() == PacketType.Play.Server.ENTITY_METADATA) {
+        } else if (tracked && event.getPacketType() == PacketType.Play.Server.ENTITY_METADATA
+                && !sendingMetadataPacket.contains(player.getUniqueId())) {
             List<WrappedWatchableObject> metadata = packet.getWatchableCollectionModifier().read(0);
             for (WrappedWatchableObject watchable : metadata) {
                 Object value = watchable.getValue();
                 if (watchable.getIndex() == 2 && value instanceof String) {
                     String parsed = PlaceholderAPI.setPlaceholders(player, line.getText());
                     watchable.setValue(parsed);
+                    sendMetadataPacket(player, packet);
                 }
             }
         }
+    }
+
+    // Avoid
+    private void sendMetadataPacket(Player player, PacketContainer packet) {
+        sendingMetadataPacket.add(player.getUniqueId());
+        try {
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet.deepClone());
+        } catch (InvocationTargetException ignored) {
+        }
+        sendingMetadataPacket.remove(player.getUniqueId());
     }
 }
